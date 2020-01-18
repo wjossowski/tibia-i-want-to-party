41914f9b5eacaca593732b571f419e5fdf9de761
Wojtek Ossowski <w.j.ossowski@gmail.com>
2020-01-18 18:07:48 +0100

    Local heroku build

--- client/src/api/request.js
+++ client/src/api/request.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export default axios.create({
-  baseURL: 'http://localhost:8080/api/v1',
+  baseURL: `http://localhost:${process.env.REACT_APP_REST_API_PORT}/api/v1`,
   responseType: 'json',
 })
