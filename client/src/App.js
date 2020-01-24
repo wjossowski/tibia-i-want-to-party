33a97a1abc7ca42caee1c44529c58d2044738023
Wojtek Ossowski <w.j.ossowski@gmail.com>
2020-01-24 17:33:03 +0100

    Add View source on GitHub Icon

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,18 @@ import './App.css'
 import 'react-notifications/lib/notifications.css'
 import { Router } from './router/Router'
 import { NotificationContainer } from 'react-notifications'
+import ForkMeOnGithub from 'fork-me-on-github'
 
 function App() {
   return (
     <div className="App">
       <Router />
       <NotificationContainer />
+      <ForkMeOnGithub
+        repo="https://github.com/mojtekossowski/tibia-i-want-to-party"
+        text="View source on GitHub"
+        side="left"
+      />
     </div>
   )
 }
--- client/src/api/request.js
+++ client/src/api/request.js
@@ -1,9 +1,11 @@
 import axios from 'axios'
 
-export default axios.create({
+const request = axios.create({
   baseURL:
     process.env.NODE_ENV === 'production'
       ? `/api/v1`
       : 'http://localhost:8080/api/v1',
   responseType: 'json',
 })
+
+export default request
